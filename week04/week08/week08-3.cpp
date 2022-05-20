b67c0cc6ba544d977816b56b7a74bdd640f9f668
vitahsu030812 <vita20140510@gmail.com>
2022-05-20 12:03:23 +0800

    add week13

--- week04/week08/week08-3.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-
-        FILE*fout=fopen("檔案輸出.txt","w+");
-        fprintf(fout,"Hellow我在這裡哦\n");
-        printf("Hellow World\n");
-}
---
+++ week13/week13_4_image/week13_4_image.pde
@@ -0,0 +1,3 @@
+size(500,500);
+PImage img = loadImage("image.jpg");
+image(img,0,0);
